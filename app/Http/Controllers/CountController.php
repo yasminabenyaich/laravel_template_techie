4c4fc62bebb78fffcf4b0925b34002f85abaa1b0
yasmianbenyaich <yasminabenyaich@outlouk.com>
2021-06-03 16:42:31 +0200

    creation des tables ressources/controller

---
+++ app/Http/Controllers/CountController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\count;
+use Illuminate\Http\Request;
+
+class CountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\count  $count
+     * @return \Illuminate\Http\Response
+     */
+    public function show(count $count)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\count  $count
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(count $count)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\count  $count
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, count $count)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\count  $count
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(count $count)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TestiomonialsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Testiomonials;
+use Illuminate\Http\Request;
+
+class TestiomonialsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Testiomonials  $testiomonials
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Testiomonials $testiomonials)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Testiomonials  $testiomonials
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Testiomonials $testiomonials)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Testiomonials  $testiomonials
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Testiomonials $testiomonials)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Testiomonials  $testiomonials
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Testiomonials $testiomonials)
+    {
+        //
+    }
+}
