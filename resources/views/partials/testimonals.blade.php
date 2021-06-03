02061dd65be32d12e995b1ea2fceab85719a1668
yasmianbenyaich <yasminabenyaich@outlouk.com>
2021-06-03 16:04:01 +0200

    backoffice

--- resources/views/partials/testimonals.blade.php
+++ resources/views/partials/testimonals.blade.php
@@ -29,7 +29,7 @@
                 Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.
                 <i class="bx bxs-quote-alt-right quote-icon-right"></i>
               </p>
-              <img src={{asset('img/testimonials/testimonials-2.jpg') }} class="testimonial-img" alt="">
+              <img src={{asset('img/testimonials/testimonials-2.jpg')}} class="testimonial-img" alt="">
               <h3>Sara Wilsson</h3>
               <h4>Designer</h4>
             </div>
@@ -42,7 +42,7 @@
                 Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.
                 <i class="bx bxs-quote-alt-right quote-icon-right"></i>
               </p>
-              <img src={{asset('img/testimonials/testimonials-3.jpg') }} class="testimonial-img" alt="">
+              <img src={{asset('img/testimonials/testimonials-3.jpg')}} class="testimonial-img" alt="">
               <h3>Jena Karlis</h3>
               <h4>Store Owner</h4>
             </div>
@@ -55,7 +55,7 @@
                 Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore.
                 <i class="bx bxs-quote-alt-right quote-icon-right"></i>
               </p>
-              <img src={{asset('img/testimonials/testimonials-4.jpg') }} class="testimonial-img" alt="">
+              <img src={{asset('img/testimonials/testimonials-4.jpg')}} class="testimonial-img" alt="">
               <h3>Matt Brandon</h3>
               <h4>Freelancer</h4>
             </div>
